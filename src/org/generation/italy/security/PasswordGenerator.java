446c54bfdea846fc04273b48e653a0bab990b54c
leonardobernardi <lbernardi4298@gmail.com>
2021-11-15 13:49:57 +0100

    finito

--- src/org/generation/italy/security/PasswordGenerator.java
+++ src/org/generation/italy/security/PasswordGenerator.java
@@ -3,7 +3,16 @@ package org.generation.italy.security;
 public class PasswordGenerator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String name = "Leonardo";
+		String surname = "Bernardi";
+		String favColor = "black";
+		String m = "-";
+		int bDay = 4;
+		int bMonth = 2;
+		int bYear = 1998;
+		int bDate = (bDay + bMonth + bYear);
+		
+		System.out.println(name + m + surname + m + favColor + m + bDate);
 
 	}
 
